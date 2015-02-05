6bfe8ea821574f26cdeb412c606f7242800703f7
Erik Chacon <erikchacon@me.com>
2015-02-05 08:38:26 -0800

    Disabling tmux color changing & fix folder creations on install.sh

--- install.sh
+++ install.sh
@@ -7,13 +7,12 @@
 # checking if .vim folder exists
 if [ -d "${HOME}/.vim" ]; then
   mv "${HOME}/.vim" "${HOME}/.vim-old"
+  # creating Symbolic Link
+  ln -s "${PWD}/.vim" "${HOME}/.vim"
 else
   echo ".vim folder does not exists - please install vim"
-  exit
 fi
 
-# creating Symbolic Link
-ln -s "${PWD}/.vim" "${HOME}/.vim"
 
 
 # 
@@ -23,10 +22,12 @@ ln -s "${PWD}/.vim" "${HOME}/.vim"
 # backup .vimrc if exists
 if [ -e "${HOME}/.vimrc" ]; then
   mv "${HOME}/.vimrc" "${HOME}/.vimrc-old"
+  # creating symbolic link
+  ln -s "${PWD}/.vimrc" "${HOME}/.vimrc"
+else
+  echo ".vim is not installed - please install vim"
 fi
 
-# creating symbolic link
-ln -s "${PWD}/.vimrc" "${HOME}/.vimrc"
 
 
 #
@@ -36,21 +37,23 @@ ln -s "${PWD}/.vimrc" "${HOME}/.vimrc"
 # backup .vimrc if exists
 if [ -e "${HOME}/.tmux.conf" ]; then
   mv "${HOME}/.tmux.conf" "${HOME}/.tmux.conf-old"
+  # creating symbolic link
+  ln -s "${PWD}/.tmux.conf" "${HOME}/.tmux.conf"
+else
+  echo "Tmux is not installe on the system - please install tmux"
 fi
 
-# creating symbolic link
-ln -s "${PWD}/.tmux.conf" "${HOME}/.tmux.conf"
 
-
-#
-# TMUX FUNCTION TO CHANGE COLOR
-#
 
 # creating dotfiles 
 ln -s "${PWD}" "${HOME}/.dotfiles"
 
+# FUNCTION DISABLED TEMPORALY 
+# BUGS TO FIX
+# TMUX FUNCTION TO CHANGE COLOR
+#
 # add tmux function to .zshrc
-if [ -e "${HOME}/.zshrc" ]; then
-  cat "${PWD}/tmux.sh" >> "${HOME}/.zshrc"
-fi
+#if [ -e "${HOME}/.zshrc" ]; then
+#  cat "${PWD}/tmux.sh" >> "${HOME}/.zshrc"
+#fi
 
